785bd5cacaa0116a0693bd44856396877ad74b5d
Harshbir Singh <166424299+harshbir026@users.noreply.github.com>
2026-07-02 22:18:47 +0530

    Add: add-two-numbers - submission-1

---
+++ Data Structures & Algorithms/add-two-numbers/submission-1.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+        int carry = 0;
+        ListNode* temp1 = l1;
+        ListNode* temp2 = l2;
+        while(temp1!=nullptr || temp2!=nullptr || carry>0){
+            int sum = carry;
+            if(temp1){
+                sum+=temp1->val;
+                temp1 = temp1->next;
+            }
+            if(temp2){
+                sum+=temp2->val;
+                temp2 = temp2->next;
+            } 
+            carry = sum / 10;
+            sum = sum % 10;
+            temp->next = new ListNode(sum);
+            temp = temp->next;
+
+        }
+        
+        return dummy->next;
+    }
+};
